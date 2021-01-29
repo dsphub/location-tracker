60aea812eddea3107bd3cdf44271bcaf186f1ec1
dsp <dsp.freelancer@gmail.com>
2021-01-29 22:50:16 +0300

    Log signal strength

--- app/src/main/java/com/dsp/androidsample/LocationManagerWrapper.kt
+++ app/src/main/java/com/dsp/androidsample/LocationManagerWrapper.kt
@@ -7,7 +7,12 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.net.wifi.WifiManager
+import android.os.Build
 import android.provider.Settings
+import android.telephony.*
+import android.telephony.CellSignalStrength.*
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import com.dsp.androidsample.log.Logger.d
 import com.dsp.androidsample.log.Logger.w
@@ -16,6 +21,7 @@ import io.reactivex.subjects.BehaviorSubject
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
+
 
 class LocationManagerWrapper(private val context: Context) {
     private var locationManager: LocationManager = context
@@ -26,10 +32,15 @@ class LocationManagerWrapper(private val context: Context) {
     private val dateFormatter = SimpleDateFormat("HH:mm:ss", Locale.US)
 
     init {
-        val provider = locationManager.getProvider(LocationManager.GPS_PROVIDER)
+        d { "init" }
     }
 
     fun isGpsEnabled() = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    private fun isNetworkEnabled() =
+        locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+    private fun isPassiveEnabled() =
+        locationManager.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)
 
     fun enableGps() {
         val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
@@ -38,26 +49,25 @@ class LocationManagerWrapper(private val context: Context) {
     }
 
     fun enable() {
-        d { "enable" }
+        val providers = Settings.Secure.getString(
+            context.contentResolver,
+            Settings.Secure.LOCATION_PROVIDERS_ALLOWED
+        )
+        d { "enable gps=${isGpsEnabled()} net=${isNetworkEnabled()} pas=${isPassiveEnabled()}" }
+        d { "providers $providers" }
+        signalStrength()
         if (ActivityCompat.checkSelfPermission(
                 context,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             w { "permission is not granted" }
             return
         }
         listener = object : LocationListenerAdapter() {
             override fun onLocationChanged(location: Location?) {
                 val c = counter.incrementAndGet()
-                d { "$c: ${dateFormatter.format(Date())} lat=${location?.latitude} lon=${location?.longitude}" }
+                d { "$c: ${dateFormatter.format(Date())} p=${location?.provider} lat=${location?.latitude} lon=${location?.longitude} acc=${location?.accuracy}" }
                 subject.onNext("$c: ${dateFormatter.format(Date())} lat=${location?.latitude} lon=${location?.longitude}")
             }
         }
@@ -79,6 +89,71 @@ class LocationManagerWrapper(private val context: Context) {
             MIN_DISTANCE_METER,
             listener
         )
+    }
+
+    private fun signalStrength() {
+        wifiSignalStrength()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            mobileSignalStrength()
+    }
+
+    private fun wifiSignalStrength() {
+        val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val rssi = wifiManager.connectionInfo.rssi
+        val level = WifiManager.calculateSignalLevel(rssi, 4)
+        d { "wifi enabled=${wifiManager.isWifiEnabled} rssi=$rssi level[0..4]=$level" }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun mobileSignalStrength() {
+        val telephonyManager =
+            context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        if (telephonyManager.allCellInfo.isEmpty()) {
+            d { "no active mobile cells" }
+        }
+        val cellSignalStrengthGsm: CellSignalStrength = when (telephonyManager.allCellInfo[0]) {
+            is CellInfoGsm -> {
+                (telephonyManager.allCellInfo[0] as CellInfoGsm).cellSignalStrength
+            }
+            is CellInfoWcdma -> {
+                (telephonyManager.allCellInfo[0] as CellInfoWcdma).cellSignalStrength
+            }
+            is CellInfoLte -> {
+                (telephonyManager.allCellInfo[0] as CellInfoLte).cellSignalStrength
+            }
+            else -> {
+                w { "unknown cell type ${telephonyManager.allCellInfo[0]}" }
+                return
+            }
+        }
+        /*
+         * Return the Received Signal Strength Indicator
+         *
+         * @return the RSSI in dBm (-113, -51) or
+         *         {@link android.telephony.CellInfo#UNAVAILABLE UNAVAILABLE}.
+         */
+        val enabled = telephonyManager.isDataEnabled
+        val rssi = cellSignalStrengthGsm.dbm
+        val asu = cellSignalStrengthGsm.asuLevel
+
+        d { "mobile enabled=${enabled} rssi=$rssi level[0..4]=${getGsmLevel(asu)}" }
+    }
+
+    /**
+     * Get gsm as level 0..4
+     *
+     * @hide
+     */
+    private fun getGsmLevel(asu: Int): Int {
+        // ASU ranges from 0 to 31 - TS 27.007 Sec 8.5
+        // asu = 0 (-113dB or less) is very weak
+        // signal, its better to show 0 bars to the user in such cases.
+        // asu = 99 is a special case, where the signal strength is unknown.
+        return if (asu <= 2 || asu == 99) SIGNAL_STRENGTH_NONE_OR_UNKNOWN
+        else if (asu >= 12) SIGNAL_STRENGTH_GREAT
+        else if (asu >= 8) SIGNAL_STRENGTH_GOOD
+        else if (asu >= 5) SIGNAL_STRENGTH_MODERATE
+        else SIGNAL_STRENGTH_POOR
     }
 
     fun disable() {
